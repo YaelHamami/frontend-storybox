c58d1a990794a6bc19a7d1a433546d74eb734e81
boazAvami <boaz.avami@gmail.com>
2025-03-15 16:17:52 +0200

    all best

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // src/App.tsx
-import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import LoginForm from './pages/LoginForm';
 import RegistrationForm from './pages/RegistrationForm';
@@ -9,7 +9,6 @@ import HomePage from './pages/HomePage';
 import NavBar from './components/NavBar'; // Import the NavBar component
 
 function App() {
-  const location = useLocation();
 
   return (
     <>
@@ -21,6 +20,7 @@ function App() {
         <Route path="/register" element={<RegistrationForm />} />
         <Route path="/profile/:userId" element={<UserProfile />} />
         <Route path="/profile/:userId/edit" element={<EditProfile />} />
+        {/* <Route path="/addPost" element={<AddPost />} /> */}
       </Routes>
     </>
   );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,23 +1,22 @@
 import { Link, useLocation } from 'react-router-dom';
-import { FaHome, FaComments, FaUser } from 'react-icons/fa'; // Importing icons
+import { FaHome, FaComments, FaUser, FaPlus, FaBook } from 'react-icons/fa';
 import './NavBar.css';
 
 const NavBar = () => {
   const location = useLocation();
 
   // Check if the current path should hide the NavBar
-  const hideNavBar = [
-    '/login',
-    '/register',
-    '/chat',
-    '/profile/edit', // Adjust if necessary
-  ].includes(location.pathname);
+  const hideNavBar = ['/login', '/register'].includes(location.pathname);
 
   if (hideNavBar) return null; // Don't render NavBar for these paths
 
   return (
     <nav className="navbar">
-      <ul className="navbar-menu">
+      <div className="navbar-left">
+        <FaBook className="navbar-icon-book" />
+        <Link to="/home" className="navbar-title">StoryBox</Link>
+      </div>
+      <ul className="navbar-right">
         <li>
           <Link to="/home" className="navbar-link">
             <FaHome className="navbar-icon" /> Home
@@ -29,8 +28,13 @@ const NavBar = () => {
           </Link>
         </li>
         <li>
-          <Link to="/profile/1" className="navbar-link"> {/* Replace with actual user ID */}
+          <Link to="/profile/" className="navbar-link">
             <FaUser className="navbar-icon" /> Profile
+          </Link>
+        </li>
+        <li>
+          <Link to="/addPost/" className="navbar-link">
+            <FaPlus className="navbar-icon" /> Add Post
           </Link>
         </li>
       </ul>
